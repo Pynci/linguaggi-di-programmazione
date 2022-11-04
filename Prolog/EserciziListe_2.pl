04bac222cf45eeca2e229586eaf48fe57a44b54d
lucapinciroli <luca.pinciroli02@gmail.com>
2022-11-04 11:10:55 +0100

    aggiunta ulteriori esercizi, da fare: inversione lista

--- Prolog/EserciziListe_2.pl
+++ Prolog/EserciziListe_2.pl
@@ -37,3 +37,36 @@ elimina(K, [X | Xs], [X | Ys]) :-
     Kn is K-1,
     K > 0,
     elimina(Kn, Xs, Ys).
+
+%! Calcola la lunghezza di una lista
+lunghezza([], 0).
+lunghezza([_ | Xs], L) :-
+    lunghezza(Xs, NewL),
+    L is NewL + 1.
+% NB: per fare sì che is funzioni è necessario che ciò che si trova alla sua
+% destra abbia un effettivo valore.
+% Per questo motivo qui bisogna mettere la chiamata ricorsiva successiva prima
+% di stabilire il valore di L nella chiamata ricorsiva precedente.
+% Se avessimo messo "NewL is L-1" prima di effetture la ricorsione si sarebbe
+% verificato un errore, in quanto il sistema non ha un valore di L da calcolare.
+% Scrivendo invece in questo modo il sistema può prima arrivare in fondo alle
+% chiamate per giungere al caso base, per poi tornare indietro e riuscire ad
+% ogni passo ad avere in mano l'effettivo valore di NewL (che gli permette poi
+% di fare il calcolo richiesto da "L is NewL+1".
+
+%! Copia lista
+copia([], []).
+copia([X | Xs], [X | Ys]) :-
+    copia(Xs, Ys).
+
+%! Inverti lista
+inverti([], []).
+% Da correggere!!
+inverti([_ | Xs], [Y | Ys]) :-
+    append(NewXs, Last, Xs),
+    Last = Y,
+    inverti(NewXs, Ys).
+    
+    
+    
+    
